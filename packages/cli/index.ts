d2a1c7a1a1148ac2250d414c921ce5d01e59a8d4
Philip Andrew Wee De Wang <philip.andrewweedewang@gmail.com>
2025-08-27 13:46:04 +0800

    made cli initially

---
+++ packages/cli/index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('lawless-supabase')
+  .description('CLI for Lawless Supabase utilities')
+  .version('1.0.0');
+
+program
+  .command('hello')
+  .description('Say hello')
+  .action(() => {
+    console.log('Hello from Lawless Supabase CLI!');
+  });
+
+program
+  .command('meta')
+  .description('PostgreSQL metadata utilities')
+  .action(() => {
+    console.log('PostgreSQL metadata functionality - coming soon!');
+  });
+
+program.parse();
